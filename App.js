70e4b66d6616673e69993cf1b878a8d47eaa8b28
Gabriel Moncea <39256258+gabrielmoncea@users.noreply.github.com>
2020-07-16 16:37:37 +0300

    Update App.js

--- App.js
+++ App.js
@@ -4,14 +4,14 @@ import { Provider } from 'react-redux';
 import Navigator from './src/Navigator';
 import store from './src/Redux/store';
 
-if (__DEV__) {
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  const ReactRedux = require('react-redux');
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-    trackExtraHooks: [[ReactRedux, 'useSelector']],
-  });
-}
+// if (__DEV__) {
+//   const whyDidYouRender = require('@welldone-software/why-did-you-render');
+//   const ReactRedux = require('react-redux');
+//   whyDidYouRender(React, {
+//     trackAllPureComponents: true,
+//     trackExtraHooks: [[ReactRedux, 'useSelector']],
+//   });
+// }
 
 const App = () => {
   return (
